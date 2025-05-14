2e80de810925aa907330b1256b0d9b343a62ff23
Bratua <digital@hakimgroup.co.uk>
2025-05-14 09:35:38 +0100

    - Fix cors error

--- server/api/index.ts
+++ server/api/index.ts
@@ -27,11 +27,7 @@ dotenv.config();
 const app: Express = express();
 app.use(cors());
 app.use(express.json({ limit: "25mb" }));
-app.use(express.urlencoded({ limit: "25mb" }));
-app.use((_req: Request, res: Response, next: NextFunction) => {
-	res.setHeader("Access-Control-Allow-Origin", "*");
-	next();
-});
+app.use(express.urlencoded({ limit: "25mb", extended: true }));
 
 const port = process.env.PORT;
 const pass = process.env.RESEND_API_KEY;
