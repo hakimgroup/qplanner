d6392262b4d3e243d1a574a982742561a7f1a007
Hakim Group <digital@hakimgroup.co.uk>
2025-11-02 14:20:09 +0100

    - Fixed icon alignment on dashboard sidebar

--- client/src/pages/dashboard/DashboardSidebar.tsx
+++ client/src/pages/dashboard/DashboardSidebar.tsx
@@ -1,63 +1,63 @@
 import {
-	Button,
-	Stack,
-	Title,
-	Divider,
-	Center,
-	Flex,
-	useMantineTheme,
-	ActionIcon,
+  Button,
+  Stack,
+  Title,
+  Divider,
+  Center,
+  Flex,
+  useMantineTheme,
+  ActionIcon,
 } from "@mantine/core";
 import {
-	IconChevronLeft,
-	IconChevronRight,
-	IconFilter,
+  IconChevronLeft,
+  IconChevronRight,
+  IconFilter,
 } from "@tabler/icons-react";
 import DashboardFilters from "./filters/DashboardFilters";
 import cl from "./dashboardSidebar.module.scss";
 
 interface SidebarProps {
-	collapsed: boolean;
-	setCollapsed: (c: boolean) => void;
+  collapsed: boolean;
+  setCollapsed: (c: boolean) => void;
 }
 
 export function DashboardSidebar({ collapsed, setCollapsed }: SidebarProps) {
-	const T = useMantineTheme();
+  const T = useMantineTheme();
 
-	return (
-		<Stack
-			h="100%"
-			gap={25}
-			pt={100}
-			pr={10}
-			pl={20}
-			w={collapsed ? 50 : 300}
-			className={cl["dashboard-sidebar"]}
-		>
-			<Flex align={"center"} justify={"space-between"}>
-				{!collapsed && (
-					<Flex align={"center"} gap={8}>
-						<IconFilter size={18} color={T.colors.blue[4]} />
-						<Title order={5} fw={600}>
-							Filters & Views
-						</Title>
-					</Flex>
-				)}
+  return (
+    <Stack
+      h="100%"
+      gap={25}
+      pt={100}
+      pr={10}
+      pl={20}
+      w={collapsed ? 50 : 300}
+      className={cl["dashboard-sidebar"]}
+    >
+      <Flex align={"center"} justify={collapsed ? "center" : "space-between"}>
+        {!collapsed && (
+          <Flex align={"center"} gap={8}>
+            <IconFilter size={18} color={T.colors.blue[4]} />
+            <Title order={5} fw={600}>
+              Filters & Views
+            </Title>
+          </Flex>
+        )}
 
-				<ActionIcon
-					variant="subtle"
-					color="violet"
-					onClick={() => setCollapsed(!collapsed)}
-				>
-					{collapsed ? (
-						<IconChevronRight size={18} color={T.colors.gray[8]} />
-					) : (
-						<IconChevronLeft size={18} color={T.colors.gray[8]} />
-					)}
-				</ActionIcon>
-			</Flex>
+        <ActionIcon
+          variant="subtle"
+          color="violet"
+          onClick={() => setCollapsed(!collapsed)}
+        >
+          {collapsed ? (
+            <IconChevronRight size={18} color={T.colors.gray[8]} />
+          ) : (
+            <IconChevronLeft size={18} color={T.colors.gray[8]} />
+          )}
+        </ActionIcon>
+      </Flex>
 
-			{!collapsed && <DashboardFilters />}
-		</Stack>
-	);
+      {!collapsed && <DashboardFilters />}
+    </Stack>
+  );
 }
